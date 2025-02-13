c4e3a6d49f6f71004d5290e49e99f5bc82d143d3
Claire W <100562199+claireaw@users.noreply.github.com>
2025-02-12 23:06:27 -0600

    added basic scraping abilities

--- logic.py
+++ logic.py
@@ -1,2 +1,10 @@
-def search(wikiname, serachtype):
-    print(str(wikiname) + ' and ' + str(serachtype))
+import requests
+from bs4 import BeautifulSoup
+
+def search(wikiname, searchtype, pagename):
+
+    page = requests.get('https://en.wikipedia.org/wiki/' + pagename)
+    # check if provided url needs to be cleaned
+    nameString = "https://" + str(wikiname)
+    newSoup = BeautifulSoup(page.content, 'html.parser')
+    print(newSoup.prettify())
--- main.py
+++ main.py
@@ -14,21 +14,23 @@ if __name__ == '__main__':
     mainCompartment.geometry('300x300')
     mainCompartment.title("WikiScraper")
 
-    mystring = tk.StringVar(mainCompartment)
+    urlString = tk.StringVar(mainCompartment)
+    pageString = tk.StringVar(mainCompartment)
 
     # set up ui input contents
     tk.Label(mainCompartment, text='Wiki URL:').place(x=80, y=10)
-    urlEnter = tk.Entry(mainCompartment, textvariable=mystring).place(x=136, y=10)
+    urlEnter = tk.Entry(mainCompartment, textvariable=urlString).place(x=136, y=10)
 
     entryBox = ttk.Combobox(state='readonly', values=searchOptions)
     entryBox.set(searchOptions[0])
     tk.Label(mainCompartment, text='Search Type:').place(x=65, y=35)
     entryBox.place(x=138, y=35)
 
-    def getvalue():
-        print(mystring.get())
+    tk.Label(mainCompartment, text='Page Name:').place(x=65, y=60)
+    pageEnter = tk.Entry(mainCompartment, textvariable=pageString).place(x=136, y=60)
 
-    submitBtn = Button(mainCompartment, text='Run Page Scraping', command=lambda: logic.search(wikiname=entryBox.get(), serachtype=mystring.get()))
+    # be sure to take input for page/category name
+    submitBtn = Button(mainCompartment, text='Run Page Scraping', command=lambda: logic.search(wikiname=urlString.get(), searchtype=entryBox.get(), pagename=pageString.get()))
     submitBtn.place(x=105, y=200)
 
     # begin ui
